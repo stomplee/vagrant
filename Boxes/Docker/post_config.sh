9586227ced1236b6820a5f69222236e1a22b8797
Stomp Lee <stomp@lee.com>
2017-09-21 11:08:39 -0700

    updated config script

--- Boxes/Docker/post_config.sh
+++ Boxes/Docker/post_config.sh
@@ -27,8 +27,10 @@ then
 
   # Test that docker works correctly by loading the hello-world image
   docker run hello-world
-
+  
+  # Install docker compose
   curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
+
   touch ~/runonce
 fi
